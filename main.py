9941c0efe99e6c47c50fa94ee2356830c85e4180
Shaheer-Pk <hasankhan.shaheer@gmail.com>
2026-06-15 16:38:17 +0500

    Worked on arc_project, tweaked some things to establish proper ARC connection

--- main.py
+++ main.py
@@ -2,8 +2,6 @@ from fastapi import FastAPI
 from dotenv import load_dotenv
 
 load_dotenv()       # Load env first because router tries to access api key on start-up
-import os
-print("KEY:", os.getenv("GEMINI_API_KEY"))
 
 from app.brain.routers import router as brain_router
 
