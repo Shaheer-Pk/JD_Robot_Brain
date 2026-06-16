c152a207d294d17fa370f4b41339ec734624e0b2
Shaheer-Pk <hasankhan.shaheer@gmail.com>
2026-06-17 01:03:36 +0500

    Tried adding a custom piper tts alongwith an api based tts (elevenlabs)

--- app/brain/routers.py
+++ app/brain/routers.py
@@ -1,10 +1,12 @@
 from fastapi import APIRouter
-from app.brain.schemas import ChatRequest, ChatResponse
-from app.brain.services import get_llm_response
+from fastapi.responses import Response
+from app.brain.schemas import ChatRequest
+from app.brain.services import get_llm_response, text_to_speech
 
 router = APIRouter()
 
-@router.post("/chat", response_model=ChatResponse)
+@router.post("/chat")
 async def chat(request: ChatRequest):
     llm_response = await get_llm_response(request.text)
-    return ChatResponse(response=llm_response)
+    audio_bytes = await text_to_speech(llm_response)
+    return Response(content=audio_bytes, media_type="audio/wav")    # wav format through piper tts (for elevenlab its mpeg)
